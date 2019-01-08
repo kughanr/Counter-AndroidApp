7df6781e5e64e10e50bf0e8597b7c26c42405a8b
kughanr <kughanravindran@gmail.com>
2019-01-08 22:01:47 +0000

    Update

--- app/src/main/java/kughan_ravindran/counter/MainActivity.kt
+++ app/src/main/java/kughan_ravindran/counter/MainActivity.kt
@@ -2,11 +2,32 @@ package kughan_ravindran.counter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var count : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        currentCount.text = "$count"
+
+        incrementButton.setOnClickListener {
+            count++
+            currentCount.text = "$count"
+        }
+
+        resetButton.setOnClickListener {
+            count = 0
+            currentCount.text = "$count"
+
+        }
+
+        decrementButton.setOnClickListener {
+            count--
+            currentCount.text = "$count"
+
+        }
     }
 }
